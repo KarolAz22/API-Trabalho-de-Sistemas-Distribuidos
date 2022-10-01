3c88cff2ff0e66931e831aa51c787681787bc515
odivanmarco <odivanmarcos2@gmail.com>
2022-10-01 17:21:36 -0300

    Consertando o auto increment das chaves primárias

--- records/models.py
+++ records/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 #model authors
 class Author(models.Model):
-    id_author = models.IntegerField(primary_key=True)
+    id_author = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
     course = models.CharField(max_length=100)
@@ -16,7 +16,7 @@ class Author(models.Model):
     gitHub  = models.URLField()
 
 class Co_advirsor(models.Model):
-    id_co_advisor = models.IntegerField(primary_key=True)
+    id_co_advisor = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
     course = models.CharField(max_length=100)
@@ -30,7 +30,7 @@ class Co_advirsor(models.Model):
     gitHub  = models.URLField()
 
 class Student(models.Model):
-    id_student = models.IntegerField(primary_key=True)
+    id_student = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
     course = models.CharField(max_length=100)
@@ -44,7 +44,7 @@ class Student(models.Model):
     gitHub  = models.URLField()
 
 class Advirsor(models.Model):
-    id_advisor = models.IntegerField(primary_key=True)
+    id_advisor = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
     course = models.CharField(max_length=100)
@@ -58,7 +58,7 @@ class Advirsor(models.Model):
     gitHub  = models.URLField()
 
 class Monograph(models.Model):
-    id_monography = models.IntegerField(primary_key=True)
+    id_monography = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     date_created = models.DateField(auto_now_add=True)
     course = models.CharField(max_length=100)
