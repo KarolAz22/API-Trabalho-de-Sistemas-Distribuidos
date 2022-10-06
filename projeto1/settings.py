7d9172c0fca8ffce5a6f5c44dc46ad9e0581502d
odivanmarco <odivanmarcos2@gmail.com>
2022-10-05 21:47:51 -0300

    Integrando o banco de dados postgres ao projeto

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'projeto1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BD_SD',
+        'USER': 'postgres',
+        'PASSWORD': 'devinhos123',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
