e164eb25298f58f4e5d16fdd84b601ddd1d8bdc2
Karolina Ivete Azevedo <karolina.azevedo@ufvjm.edu.br>
2022-10-21 21:30:22 -0300

    criação das views de cadastro

--- records/urls.py
+++ records/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
   path('authors' , views.registerOfAuthors, name='authors'),
-  path('co-authors' , views.registerOfCoAuthors, name='co-authors'),
+  path('co-authors' , views.registerOfCoAdvisor, name='co-authors'),
   path('students' , views.registerOfStudents, name='students'),
   path('monographs' , views.registerOfmonographs, name='monographs'),
   ]
--- records/views.py
+++ records/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from pages.forms import AuthorForms
+from pages.forms import AuthorForms, Co_advirsorForms, MonographForms, StudentForms
 
 
 def registerOfAuthors(request):
@@ -15,14 +15,46 @@ def registerOfAuthors(request):
             
     return render(request, 'registerauthors.html', {'form': form})
 
-def registerOfCoAuthors():
-    pass
+def registerOfCoAdvisor(request):
+    if request.method == "POST":
+        form = Co_advirsorForms(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.save()
+            form = AuthorForms()
+            return render(request, 'registerauthors.html', {'form': form})
+    else:
+        form = Co_advirsorForms()
+            
+    return render(request, 'registercoadvisor.html', {'form': form})
 
-def registerOfStudents():
-    pass
 
-def registerOfmonographs():
-    pass
+def registerOfStudents(request):
+    if request.method == "POST":
+        form = StudentForms(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.save()
+            form = StudentForms()
+            return render(request, 'registerstudents.html', {'form': form})
+    else:
+        form = StudentForms()
+            
+    return render(request, 'registercoadvisor.html', {'form': form})
+
+def registerOfmonographs(request):
+    if request.method == "POST":
+        form = MonographForms(request.POST)
+        if form.is_valid():
+            post = form.save()
+            post.save()
+            form = MonographForms()
+            return render(request, 'registermonograph.html', {'form': form})
+    else:
+        form = MonographForms()
+            
+    return render(request, 'registercoadvisor.html', {'form': form})
+
 
 
 
