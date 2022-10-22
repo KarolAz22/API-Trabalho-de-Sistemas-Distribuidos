79a4f427e8745fba5bb974a7e3ee799aada274f9
odivanmarco <odivanmarcos2@gmail.com>
2022-10-21 21:16:45 -0300

    Adicionando as modelForms dos outros campos

--- pages/forms.py
+++ pages/forms.py
@@ -6,4 +6,23 @@ class AuthorForms(ModelForm):
     class Meta: 
         model = Author
         fields = "__all__"
-        
+
+class Co_advirsorForms(ModelForm):
+    class Meta: 
+        model = Author
+        fields = "__all__"
+
+class Advirsor(ModelForm):
+    class Meta: 
+        model = Author
+        fields = "__all__"
+
+class StudentForms(ModelForm):
+    class Meta: 
+        model = Author
+        fields = "__all__"
+
+class MonographForms(ModelForm):
+    class Meta: 
+        model = Author
+        fields = "__all__"
