ee602d5d5fca36bd09c327749b7097867ec910bc
odivanmarco <odivanmarcos2@gmail.com>
2022-10-24 17:56:09 -0300

    Mudando o name do Banco de Dados

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'projeto1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BD_SD',
+        'NAME': 'monographs',
         'USER': 'postgres',
         'PASSWORD': 'devinhos123',
         'HOST': 'localhost',
