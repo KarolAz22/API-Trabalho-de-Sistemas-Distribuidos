13911b790cd054bd27e892fceed4835f0111d605
Karolina Ivete Azevedo <karolina.azevedo@ufvjm.edu.br>
2022-12-15 15:23:43 -0300

    consulta API

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from records.models import Monograph, Author, Advisor, Co_advisor, Student
 from .forms import MonographForms
+import requests
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -12,37 +13,84 @@ def search(request):
 
     if(select == 'title'):
         monograph = Monograph.objects.filter(title__icontains = result)
+        try:
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(title__icontains = monograph.json()['title'])
+        except(IndexError):
+            monograph=[]
+        #monograph = Monograph.objects.filter(title__icontains = result)
     elif(select == 'course'):
         monograph = Monograph.objects.filter(course__icontains = result)
+        try:
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(course__icontains = monograph.json()['course'])
+        except(IndexError):
+            monograph=[]
+        #monograph = Monograph.objects.filter(course__icontains = result)
     elif(select == 'university'):
         monograph = Monograph.objects.filter(university__icontains = result)
+        try:
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(university__icontains = monograph.json()['university'])
+        except(IndexError):
+            monograph=[]
+        #monograph = Monograph.objects.filter(university__icontains = result)
     elif(select == 'keywords'):
         monograph = Monograph.objects.filter(key_words__icontains = result)
+        try:
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(key_words__icontains = monograph.json()['key_words'])
+        except(IndexError):
+            monograph=[]
+        #monograph = Monograph.objects.filter(key_words__icontains = result)
     elif(select == 'abstract'):
         monograph = Monograph.objects.filter(abstract__icontains = result)
+        try:
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(abstract__icontains = monograph.json()['abstract'])
+        except(IndexError):
+            monograph=[]
+        #monograph = Monograph.objects.filter(abstract__icontains = result)
     elif(select == 'author'):
+
         try:
             author = Author.objects.get(name=result)
             monograph = Monograph.objects.filter(fk_id_author = author.id_author)
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(title__icontains = monograph.json()['title'])
         except:
-            monograph = []
+            monograph=[]
+       
     elif(select == 'advisor'):
         try:
             advisor = Advisor.objects.get(name=result)
             monograph = Monograph.objects.filter(fk_advisor = advisor)
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(title__icontains = monograph.json()['title'])
         except:
-            monograph = []
+            monograph=[]
     elif(select == 'coadvisor'):
         try:
             coadvisor = Co_advisor.objects.get(name=result)
             monograph = Monograph.objects.filter(fk_co_advisor = coadvisor)
+            id = monograph[0].id_monography
+            monograph = requests.get("http://localhost:8000/api/register-monograph/{}/?format=json".format(id))
+            monograph = Monograph.objects.filter(title__icontains = monograph.json()['title'])
         except:
-            monograph = []
+            monograph=[]
             
     if(len(monograph) > 0):
         return render(request, 'home.html', {'monographs': monograph})
     else:
         return render(request, 'home.html', {'message': "Nenhum resultado encontrado"})
+    
 
 def details(request, pk):
     try:
